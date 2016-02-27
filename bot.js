5176ef25c4a10b68ca74262ed8c88517d515cd92
ruiduarte86 <ruibritocamacho@gmail.com>
2016-02-27 01:06:23 +0000

    first commit

---
+++ bot.js
@@ -0,0 +1,50 @@
+var TelegramBot = require('node-telegram-bot-api');
+var token = '208068117:AAGawS2TwQpJjUj3pHNl6gfEXGiz4ASEgS4';
+
+var Q = require('q');
+var request = Q.denodeify(require("request"));
+
+var bot = new TelegramBot(token, {polling: true});
+bot.getMe().then(function (me) {
+  console.log('Hi my name is %s!', me.username);
+});
+
+//matches /start
+bot.onText(/\/start/, function (msg, match) {
+  var fromId = msg.from.id; // get the id, of who is sending the message
+  var message = "Hello and welcome. I'm Poppy\n"
+  message += "Get weather update by sending /weather [your_city] command."
+  bot.sendMessage(fromId, message);
+});
+
+// match /weather [whatever]
+bot.onText(/\/weather (.+)/, function (msg, match) {
+  var fromId = msg.from.id; // get the id, of who is sending the message
+  var postcode = match[1];
+  getWeatherData(postcode)
+  .then(function(data){
+    var message = "Weather today in "+postcode+"\n";
+    message += data.wx_desc+"\n"
+    message += "temp: "+data.temp_c+"C or "+data.temp_f+"F"
+    bot.sendMessage(fromId, message);
+  });
+
+});
+
+function getWeatherData(postcode){
+  var app_id = "c2a2c786"
+  var app_key = "5bababed824382c7495424f8ca976135"
+  var url = "http://api.weatherunlocked.com/api/current/us."+postcode
+  url += "?app_id="+app_id+"&app_key="+app_key
+
+  var options ={
+      url: url,
+      method: "GET",
+      json:true,
+    }
+    var response = request(options);
+    return response.then(function (r){
+        return r[0].body
+    })
+}
+
